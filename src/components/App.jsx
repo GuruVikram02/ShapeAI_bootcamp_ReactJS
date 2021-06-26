edd0fc875fb374daa44a3d7692de93d366950e3f
Guru vikram <65849638+GuruVikram02@users.noreply.github.com>
2021-06-26 18:40:39 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Headder from "./Headder";
+import Footer from "./Footer";
+import info from "./Info";
+function App(){
+    return (
+        <div>
+        <Headder />
+        <Footer />
+        <Info />
+        </div>
+
+    );
+}
+
+export default App;
---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function info(){
+    return (
+    <div class="note">
+    <h1>Copyright by ShapeAI @{new Date.getFullYear()}</h1>
+    <p>A basic web dev React JS bootcamp</p>
+    </div>
+
+    );
+}
+
+export default info;
