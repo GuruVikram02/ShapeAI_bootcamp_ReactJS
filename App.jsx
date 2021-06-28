821004590802d91506aae8d88acb4630fddeac08
GuruVikram02 <guru.vikram.399@gmail.com>
2021-06-28 09:55:15 +0530

    Codes uploaded

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Headder from "./Headder";
+import Footer from "./Footer";
+import Info from "./Info";
+function App() {
+  return (
+    <div>
+      <Headder />
+      <Footer />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ Headder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Headder(){
+    return (
+        <header>
+            <h1>ShapeAI Bootcamp</h1>
+        </header>
+
+    );
+}
+
+export default Headder;
